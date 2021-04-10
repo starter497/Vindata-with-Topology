f17d36993d2998e7a4df25c31933a997183e1df5
starter497 <62881115+starter497@users.noreply.github.com>
2021-04-10 18:34:25 -0400

    Add files via upload

--- test-pc/Test_GudhiDiagram.py
+++ test-pc/Test_GudhiDiagram.py
@@ -1,1 +1,230 @@
+# -*- coding: utf-8 -*-
+"""
+Created on March 20th
+
+@author: cnem modified Neils original code
+
+"""
+
+import os
+
+import numpy as np
+
+import gudhi  #create complexes
+
+import matplotlib
+
+from PIL import Image, ImageOps
+
+from matplotlib import pyplot as plt
+
+import persistencecurves as pc # vectorization.  Be sure to pip install import
+
+import pydicom #converts dicom images to PIL. Be sure to pip install import
+
+from pydicom_PIL import show_PIL #just to show the image
+
+import warnings
+
+warnings.filterwarnings("ignore")
+
+usetex = matplotlib.checkdep_usetex(True) #I dont have latex)
+
+
+# -------------- Functions -----------------
+
+def Avg_Total_lifespan(D,x,y,t): # D= diagram, x = birth value, y = death value, t= input
+    return (y-x)*t
+
+def stat(A):
+    if len(A) ==0 :
+        return 0
+    else:
+        return np.mean(A)
+
+
+
+
+
+
+
+
+
+
+#--------------------Configuration---------------------
+
+
+path = r"/vindata/test/"
+
+testfilename = os.listdir(path)
+
+
+
+print("Number of train files:",len(testfilename))
+
+image_dicom =  []
+image = []
+
+'''
+ALL_PD0 = []    #all of the persistent diagram information 0dimension
+ALL_PD1 = []    #all of persistent diagram info 1dimension
+BC0 = []       #all of betticurve 0-dim
+BC1 = []       #all of betticurve 1-dim
+G0 = []
+G1 = []
+ATL0 = []
+ATL1 = []
+'''
+
+for j in range(len(testfilename)):
+
+     #remove comment if you want to make a directory for saving plots(not necessary)
+    
+    ID = os.path.splitext(testfilename[j])[0]
+    
+
+    results_dir = os.path.dirname(__file__)
+
+    results_dir = os.path.join(results_dir, ID + "-pc" )
+
+    if not os.path.isdir(results_dir):
+
+       os.makedirs(results_dir)
+    
+
+    image_dicom.append(pydicom.dcmread(path + testfilename[j]))
+
+    image = image_dicom[j].pixel_array
+
+    max_pixel = np.amax(image)
+
+    ''' We are not rescaling so leave this uncommented
+
+    # -----------------Rescaling Pixel intensity to 0-255 ---------------
+    
+
+    image = image.astype(float) # gets floating point numbers for values
+
+    image = (np.maximum(image,0) / image.max()) * 255.0 #Rescales to grayscaled between 0-255 rather than 3000-65,535.  Too much intensity for memory
+
+    image = np.uint8(image) 
+   
+    '''
+    
+   
+   # print(image.shape) #Note NOT ALL IMAGES ARE SAME SIZE
+
+
+   #print(image_dicom[j])  #if you want to see the meta-data
+
+   #show_PIL(image_dicom[j]) #if you want to see the images
+
+
+
+    reshaped = np.reshape(image, [image.shape[0]*image.shape[1]], order = 'F')
+    # reshapes image to a single vector
+
+    Complex = gudhi.CubicalComplex(dimensions=image.shape, top_dimensional_cells=reshaped)
+    # creates the cubical complex from the image
+
+    Complex.persistence()
+
+    Dgm0 = Complex.persistence_intervals_in_dimension(0)
+    # compute oth dimensional persistence diagram
+
+    Dgm1 = Complex.persistence_intervals_in_dimension(1)
+    
+    
+    print(j) #tells you the progress, finish at 15,000
+
+
+    
+    #ALL_PD0.append(Dgm0) #This is the array for all of the persistent diagrams
+    #ALL_PD1.append(Dgm1) #No need to store.
+    
+    
+
+
+
+
+#-------------Using 0- PersistentDiagram to curve info ---------
+    
+   
+    D0 = pc.Diagram(Dgm = Dgm0, globalmaxdeath = None, infinitedeath=None, inf_policy="keep")
+
+
+
+    D1 = pc.Diagram(Dgm = Dgm1,globalmaxdeath = None, infinitedeath = None, inf_policy= "keep")
+
+
+#---------------------0 and 1 dimensional Betti curve (number of points in Fundamental box) info ----------------------
+
+
+    Betti0 = D0.normalizedBetticurve(meshstart=0,meshstop= 256 ,num_in_mesh= 256)
+
+    Betti1 = D1.normalizedBetticurve(meshstart=0,meshstop= 256, num_in_mesh = 256)
+
+    
+
+#-----------------------0 and 1 dimensional Guass curve info ----------------------
+
+
+
+    Gauss0= D0.gaussian_life(meshstart=0, meshstop= 256,num_in_mesh=256, spread = 1)
+
+    Gauss1= D1.gaussian_life(meshstart=0, meshstop= 256, num_in_mesh=256, spread = 1)
+
+       
+
+
+
+#---------------0 and 1 dimensional average Normalized Total lifespan curve info -------
+
+
+    AvgTotallife0 = D0.custom_curve(Avg_Total_lifespan, stat , 0 , 256, 256)
+   
+    AvgTotallife1 = D0.custom_curve(Avg_Total_lifespan , stat , 0,  256, 256)
+
+    
+
+    np.savez_compressed( results_dir + "/NormalizedBetti_0.npz",Betti0)
+    #np.savez_compressed('data.npz',ALL_PD0)
+    
+    np.savez_compressed(results_dir + "/NormalizedBetti_1.npz",Betti1)
+    np.savez_compressed(results_dir + "/Gauss_0.npz", Gauss0)
+    np.savez_compressed(results_dir +"/Gauss_1.npz", Gauss1)
+    
+
+
+
+'''
+ #uncomment to plot betticurves
+    
+    gudhi.plot_persistence_diagram(Dgm0)
+
+    x_1 = np.linspace(0,255,num= 256) 
+
+    
+
+    plt.savefig(results_dir + "/Persistence Diagram .png")
+ 
+    plt.clf()
+ 
+    
+
+    plt.plot(x_1, dgm0)
+
+
+
+    
+
+    plt.savefig(results_dir + "/BettiCurve.png")
+    
+    plt.clf()
+'''
+
+ 
+
+print("Processing has finished! ")
+
 
