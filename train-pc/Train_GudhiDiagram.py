d41112a36595b7bd4cb889d5a721cbd5fb55a8e8
starter497 <62881115+starter497@users.noreply.github.com>
2021-04-10 20:03:03 -0400

    Add files via upload

--- train-pc/Train_GudhiDiagram.py
+++ train-pc/Train_GudhiDiagram.py
@@ -149,9 +149,9 @@ for j in range(len(trainfilename)):
 #---------------------0 and 1 dimensional Betti curve (number of points in Fundamental box) info ----------------------
 
 
-    Betti0 = D0.normalizedBetticurve(meshstart=0,meshstop= 256 ,num_in_mesh= 256)
+    Betti0 = D0.normalizedBetticurve(meshstart=0,meshstop= max_pixel+1 ,num_in_mesh= max_pixel +1)
 
-    Betti1 = D1.normalizedBetticurve(meshstart=0,meshstop= 256, num_in_mesh = 256)
+    Betti1 = D1.normalizedBetticurve(meshstart=0,meshstop= max_pixel+1, num_in_mesh = max_pixel+1)
 
     
 
@@ -159,9 +159,9 @@ for j in range(len(trainfilename)):
 
 
 
-    Gauss0= D0.gaussian_life(meshstart=0, meshstop= 256,num_in_mesh=256, spread = 1)
+    Gauss0= D0.gaussian_life(meshstart=0, meshstop= max_pixel+1,num_in_mesh=max_pixel+1, spread = 1)
 
-    Gauss1= D1.gaussian_life(meshstart=0, meshstop= 256, num_in_mesh=256, spread = 1)
+    Gauss1= D1.gaussian_life(meshstart=0, meshstop= max_pixel+1, num_in_mesh=max_pixel+1, spread = 1)
 
        
 
@@ -170,9 +170,9 @@ for j in range(len(trainfilename)):
 #---------------0 and 1 dimensional average Normalized Total lifespan curve info -------
 
 
-    norm_lifespan0 = D0.normalizedlifecurve(0 , 256, 256)
+    norm_lifespan0 = D0.normalizedlifecurve(0 , max_pixel+1, max_pixel+1)
    
-    norm_lifespan1 = D1.normalizedlifecurve(0,  256, 256)
+    norm_lifespan1 = D1.normalizedlifecurve(0,  max_pixel+1, max_pixel+1)
 
     
 
